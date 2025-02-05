d4d0f2069f05dbec268ad02e5386f35621dba76b
Joanna Mijares <127276862+jonana-split@users.noreply.github.com>
2025-02-05 02:35:30 -0500

    Memory Manager C++

---
+++ MemoryManager/MemoryManager.cpp
@@ -0,0 +1,578 @@
+#include "MemoryManager.h"
+#include <iostream>
+#include <functional>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <algorithm>
+#include <bitset>
+#include <vector>
+
+using namespace std;
+
+//
+// Joanna Mijares
+//
+//I'm very very grateful for my extensions, it helped me out so much. Thank you, Professor!
+//and thank you to the TAs who helped me out :)
+
+// do initialize, shutdown, allocate, free, getList and the allocators FIRST
+
+MemoryManager::MemoryManager(unsigned wordSize, function<int(int, void *)> allocator){
+  this-> wordSize = wordSize;
+  this-> defaultAllocator = allocator;
+}
+//store wordsize
+//store passed in allocator func as member var
+
+MemoryManager::~MemoryManager(){
+        shutdown();
+        wordSize = 0;
+        defaultAllocator = nullptr;
+}
+//deleted heap mem
+//never called directly
+//if obj initialized, call shutdown
+
+void MemoryManager::initialize(size_t sizeInWords){
+
+    if(sizeInWords > 65536) {
+        return;
+    }
+
+    if(head!=nullptr) {
+        shutdown();
+    }
+    if(memory!=nullptr){
+        delete[] memory;
+    }
+
+    this->sizeinwords = sizeInWords;
+    this->wordSize = wordSize;
+    this-> defaultAllocator = defaultAllocator;
+
+    int sizeInBytes = sizeInWords * wordSize;
+
+    memory = new uint8_t[sizeInBytes];
+
+    head = new Block(sizeInBytes, 0, true);
+    
+}
+//check documentation
+
+//CITE: https://www.geeksforgeeks.org/write-a-function-to-delete-a-linked-list/
+//CITE: https://stackoverflow.com/questions/52691072/created-a-linked-list-but-how-to-delete-all-the-nodes-in-c
+//CITE: https://www.alphacodingskills.com/cpp/ds/cpp-delete-all-nodes-of-the-doubly-linked-list.php
+void MemoryManager::shutdown(){
+
+    //blocks!
+
+    while(head != nullptr) {
+        Block* current = head->next;
+        delete head;
+        head = current;
+    }
+
+    //member vars!
+    head = nullptr;
+    sizeinwords = 0;
+    //wordSize = 0; //might move to destructor
+    delete[] memory;
+    memory = nullptr;
+    //defaultAllocator = nullptr;
+
+}
+//clears block data, frees heap mem, resets mem vars, clear data structures
+
+void *MemoryManager::allocate(size_t sizeInBytes){
+    
+    size_t numWords;
+
+    if(wordSize == 0){
+        return 0;
+    }
+
+    if(sizeInBytes % wordSize > 0 ) {
+         numWords = (sizeInBytes / wordSize) + 1;
+    }else {
+        numWords = sizeInBytes / wordSize;
+    }
+
+    //CITE: https://stackoverflow.com/questions/2622441/c-integer-floor-function
+    uint16_t* arr = static_cast<uint16_t *>(getList());
+
+    if(arr == nullptr) {
+        delete[] arr;
+        return nullptr;
+    }
+
+    int offset = defaultAllocator(numWords, arr);
+    int offsetBytes = offset*wordSize;
+
+    Block* current = head;
+
+    //CITE: https://www.geeksforgeeks.org/delete-a-node-in-a-doubly-linked-list/
+    //CITE: https://stackoverflow.com/questions/23718785/how-to-change-linkedlist-head-pointer-globally-and-not-locally
+
+    while(current != nullptr) {
+
+
+        if(offset == current->first && sizeInBytes == current->size && current->isfree == true) {
+            current->isfree = false;
+            break;
+        }else if(offset == current->first && sizeInBytes < current->size && current->isfree == true){
+            //if at the start!  
+            Block *newBlock = new Block(sizeInBytes, offset, false);
+
+            //this little if statement fixed so many issues ;-;
+            if(current == head){
+                head = newBlock;
+            }
+
+            newBlock->next = current;
+            newBlock->prev = current->prev;
+
+            if(current->prev != nullptr){
+                current->prev->next = newBlock;
+            }
+
+            current->prev = newBlock;
+            current->first = current->first + numWords;
+            current->size = current->size - sizeInBytes;
+
+            break;
+
+        }else if(offset > current->first && sizeInBytes < current->size && current->first + (current->size/wordSize) - numWords == offset && current->isfree == true){
+            //the end!
+
+            Block *newBlock = new Block(sizeInBytes, offset, false);
+            newBlock->next = current->next;
+            current->next->prev = newBlock;
+            newBlock->prev = current;
+            current->next = newBlock;
+            current->size = current->size - sizeInBytes;
+        
+            break;
+
+        }else if(offset > current->first && offset < current->next->first && sizeInBytes < current->size && current->first + (current->size/wordSize) - numWords < offset && current->isfree == true){
+            //in the middle
+
+            Block *newBlock = new Block(sizeInBytes, offset, false);
+            Block *endBlock = new Block(current->size-sizeInBytes, offset+numWords, true);
+            current->next->prev = endBlock;
+            endBlock->next = current->next;
+            current->next = newBlock;
+            newBlock->next = endBlock;
+            newBlock->prev = current;
+            endBlock->prev = newBlock;
+            current->size = current->size - sizeInBytes - endBlock->size;
+        
+            break;
+        }
+
+        current = current->next;
+    }
+
+    //nothing to allocate :(
+    if(current == nullptr){
+        delete[] arr;
+        return nullptr;
+    }
+
+    //delete!
+    delete[] arr;
+
+    return (void*)(memory + offset*wordSize);
+
+    //bytes "rented out"
+    //allocate more and more heap memory
+    //free the memory getlist allocated (delete)
+    //return a ptr in mem block to starting location of newly allocated space
+}
+
+
+//CITE: https://www.geeksforgeeks.org/cpp-program-for-deleting-a-node-in-a-linked-list/
+//CITE: https://www.geeksforgeeks.org/delete-a-node-in-a-doubly-linked-list/
+//CITE: https://www.alphacodingskills.com/cpp/ds/cpp-doubly-linked-list-traversal.php
+//CITE: https://stackoverflow.com/questions/70468653/traverse-a-doubly-linked-list-to-a-given-index
+//CITE: https://stackoverflow.com/questions/40219833/removing-nodes-from-linked-list-not-working-properly
+
+void MemoryManager::free(void *address){
+
+    Block* current = head;
+
+    //for every node/block...
+    while(current != nullptr) {
+
+        //if the address to free is within the current block (greater than start of block, less than end of block)
+        if(address >= (void*)(memory+(current->first*wordSize)) && address < (void*)((memory+(current->first*wordSize)) + current->size)) {
+
+            //no neighbors
+            if(current->prev != nullptr && current->next != nullptr && current->next->isfree == false && current->prev->isfree == false){
+                current->isfree = true;
+                return;
+            }
+
+            //if its the head + no neighbors
+            if(head == current && current->next != nullptr && current->next->isfree == false){
+                current->isfree = true;
+                return;
+            }
+
+            //if next is hole
+            if(current->next != nullptr && current->next->isfree == true) {
+                current->isfree = true;
+                Block* temp = current->next;
+                current->size += temp->size;
+                current->next = temp->next;
+                if(temp->next != nullptr){
+                    temp->next->prev = current;
+                }
+                delete temp;
+              
+            }
+            //if prev is hole
+            if(current->prev != nullptr && current->prev->isfree == true) {
+                current->isfree = true;
+                Block* temp = current->prev;
+                current->first = temp->first;
+                current->size += temp->size;
+                if(temp == head){
+                    head = current;
+                }else{
+                    current->prev = temp->prev;
+                }
+                if(temp->prev != nullptr){
+                    temp->prev->next = current;
+                }
+                delete temp;
+                
+            }
+           
+        }
+
+        if(current->next!=nullptr){
+            current = current->next;
+        }else{
+            return;
+        }
+    }
+    //TO DO:
+    // addr is starting loc of space to be freed
+    //will match smth from allocate in the last
+    //if block freed, prev hole gets extended
+    //freeing block in middle != new hole
+    return;
+}
+
+void MemoryManager::setAllocator(function<int(int, void *)> allocator){
+    this->defaultAllocator = allocator;
+}
+
+int MemoryManager::dumpMemoryMap(char *filename){
+
+    //CITE: https://www.geeksforgeeks.org/input-output-system-calls-c-create-open-close-read-write/
+    
+    int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0777);
+
+    if(fd == -1) {
+        perror("open failed");
+        return -1;
+    }
+
+    uint16_t* arr = static_cast<uint16_t *>(getList());
+
+    //CITE: https://stackoverflow.com/questions/11063748/output-commas-between-elements
+    //CITE: https://stackoverflow.com/questions/35185503/how-to-write-to-a-file-using-open-and-printf
+    //CITE: https://stackoverflow.com/questions/47874098/writing-a-list-of-integers-to-a-file-using-write
+    //CITE: https://cplusplus.com/reference/cstdio/sprintf/
+
+    char buffer[1024];
+    int strSize;
+
+    for(int i=1; i<=(2*arr[0]); i++) {
+
+        if(i % 2 == 0 && i != arr[0]*2) {
+            strSize = sprintf(buffer, "[%d, %d] - ", arr[i-1], arr[i]);
+            write(fd, buffer, strSize);
+        }else if(i%2 == 0 && i == arr[0]*2) {
+            strSize = sprintf(buffer, "[%d, %d]", arr[i-1], arr[i]);
+            write(fd, buffer, strSize);
+        }
+    }
+
+    close(fd);
+
+    delete[] arr;
+
+
+  return 0;
+}
+
+//getlist.. scary
+void *MemoryManager::getList(){
+
+    if(!head) {
+        return nullptr;
+    }
+
+    if(!memory){
+        return nullptr;
+    }
+
+    int numHoles = 0;
+
+    Block* current = head;
+
+    //go through every block
+    while (current != nullptr) {
+        //number of holes AKA arr[0]
+        if(current->isfree){
+            numHoles ++;
+        }
+        current = current->next;
+    }
+
+    //2 bit array to return
+    uint16_t* arr = new uint16_t[(2*numHoles)+1];
+
+    if(arr == nullptr){
+        return nullptr;
+    }
+
+    arr[0] = numHoles;
+
+    if(numHoles == 0){
+        return arr;
+    }
+    
+    //add values to arr
+    //this is incorrect rn
+    Block* curr = head;
+
+    int arrIndex = 1;
+
+    while (curr != nullptr) {
+
+        int arrSize = 0;
+        
+        if(wordSize != 0 && curr->size !=0){
+            arrSize = (curr->size)/wordSize;
+        }
+
+        if(curr->isfree == true){
+            arr[arrIndex] = curr->first;
+            arrIndex ++;
+            arr[arrIndex] = arrSize;
+            arrIndex ++;
+        }
+
+        if(curr->next!=nullptr){
+            curr = curr->next;
+        }else{
+            return arr;
+        }
+    }
+
+    return arr;
+ }
+
+//CITE: https://stackoverflow.com/questions/38892455/initializing-an-array-of-zeroes
+//CITE: https://stackoverflow.com/questions/19688833/iterate-over-full-array-starting-at-any-position-using-only-one-variable
+void *MemoryManager::getBitmap(){
+
+    uint16_t* arr = static_cast<uint16_t *>(getList());
+
+    uint16_t sizeofarr = arr[arr[0]*2-1]+arr[arr[0]*2];
+
+    int remainder = sizeofarr%8;
+
+    string arrayStr = "";
+
+    //assign sizeofarr if theres a remainder
+    if(sizeinwords%8 != 0){
+        sizeofarr = sizeofarr+(8-remainder);
+    }
+
+    uint16_t bs[sizeofarr]= {0};
+
+    uint16_t numBits = sizeofarr/8;
+    
+    for(int i=1; i<=arr[0]*2; i++){
+        if(i%2 == 0){
+            for(int j=arr[i-1]; j<arr[i]+arr[i-1]; j++){
+                bs[j] = 1;
+            }
+        }
+    }
+
+    for(int i=0; i<sizeofarr; i++){
+        arrayStr += to_string(bs[i]);
+    }
+
+    for(int i=0; i<arrayStr.size(); i++){
+        if(arrayStr[i] == '1'){
+            arrayStr[i] = '0';
+        }else if(arrayStr[i] == '0'){
+            arrayStr[i] = '1';
+        }
+
+    }
+    
+    uint16_t offset = 0;
+
+    //CITE: https://stackoverflow.com/questions/50972196/how-to-reverse-a-substring-of-a-string-in-c
+
+    for(uint16_t i=0; i<numBits; i++){
+        reverse(arrayStr.begin()+offset, arrayStr.begin() + offset + 8);
+        offset += 8;
+    }
+
+    //CITE: https://www.geeksforgeeks.org/program-decimal-binary-conversion/
+    string numBitsStr;
+    while(numBits){
+        if(numBits & 1){
+            numBitsStr += '1';
+        }else{
+            numBitsStr += '0';
+        }
+        numBits >>= 1;
+    }
+
+    while(numBitsStr.length() != 16){
+        numBitsStr = '0'+numBitsStr;
+    }
+
+    //CITE: https://stackoverflow.com/questions/8468597/prepend-stdstring
+    for(int i=0; i<8; i++){
+        numBitsStr.insert(numBitsStr.length(), 1 , numBitsStr[0]);
+        numBitsStr.erase(0,1);
+    }
+
+    arrayStr.insert(0, numBitsStr);
+
+    //CITE: https://stackoverflow.com/questions/2204176/how-to-initialise-memory-with-new-operator-in-c
+
+    uint16_t tracker = 0;
+
+    //CITE: https://www.geeksforgeeks.org/program-binary-decimal-conversion/?ref=next_article
+
+    //for every byte...
+
+    size_t sizeofmap = (arrayStr.size())/8;
+    uint8_t* finalBitMap = new uint8_t[sizeofmap]();
+
+    for(int i=0; i<arrayStr.size() && i/8 < sizeofmap; i+=8){
+        
+        //every group of 8 characters...
+        string sub = arrayStr.substr(i, 8);
+
+        size_t num = 0;
+        uint16_t pw = 0;
+        int length = sub.length();
+    
+        for(int j=length-1; j>=0; j--){
+            if(sub[j] == '1'){
+                num += (1<<pw);
+            }
+            pw++;
+            
+        }
+                
+        finalBitMap[i/8] = num;
+        
+    }
+
+    //attempt to solve memory issues
+    uint8_t* FINALfinalBitMap = new uint8_t[sizeofmap]();
+
+    copy(finalBitMap, finalBitMap, FINALfinalBitMap);
+
+    delete[] finalBitMap;
+    delete[] arr;
+
+    return (uint8_t*)FINALfinalBitMap;
+    
+}
+
+
+unsigned MemoryManager::getWordSize(){
+    return wordSize;
+}
+
+void *MemoryManager::getMemoryStart(){
+    return memory;
+}
+
+unsigned MemoryManager::getMemoryLimit(){
+    unsigned sizeInBytes = wordSize * sizeinwords;
+    return sizeInBytes;
+    //total bytes u can store
+}
+
+int bestFit(int sizeInWords, void *list){
+
+    //CITE: https://stackoverflow.com/questions/27852372/void-typed-function-parameter
+    
+    uint16_t* arr = (uint16_t*)list;
+
+    int sizeinbytes = sizeInWords;
+
+    if(!arr) {
+        return -1;
+    }else if(arr[0] == 0){
+        return -1;
+    }
+
+    int offset = -1;
+    int smallestHole = arr[2];
+    //starting at arr[2] because it's the first length value
+
+    for(int i=1; i <= 2*arr[0] ; i++) {
+        if(i % 2 == 0) {
+            if(arr[i]<=smallestHole && arr[i]>=sizeInWords) {
+                smallestHole = arr[i];
+                offset = arr[i-1];
+            }
+        }
+    }
+    //list is like getlist
+    //hole in list that best fits (aka smallest hole)
+    //returns word offset (aka num before size)
+
+    return offset;
+}
+
+int worstFit(int sizeInWords, void *list){
+
+    uint16_t* arr = (uint16_t*)list;
+
+    if(!arr) {
+        return -1;
+    }else if(arr[0] == 0){
+        return -1;
+    }
+
+    int offset = -1;
+    int largestHole = arr[2];
+    //starting at arr[2] because it's the first length value
+
+    for(int i=1; i <= 2*arr[0] ; i++) {
+        if(i % 2 == 0) {
+            if(arr[i]>=largestHole && arr[i]>=sizeInWords) {
+                largestHole = arr[i];
+                offset = arr[i-1];
+            }
+        }
+    }
+
+    //list like getList
+    //hole in list that is largest
+
+    return offset;
+
+}
+
---
+++ MemoryManager/MemoryManager.h
@@ -0,0 +1,50 @@
+#include <cstdint>
+#include <functional>
+#include <iostream>
+using namespace std;
+
+// linked list, stores blocks/holes
+//CITE: https://www.geeksforgeeks.org/doubly-linked-list/
+struct Block {
+  int size;
+  uint16_t first;
+  bool isfree;
+
+  Block* prev;
+  Block* next;
+
+  Block(int x, uint16_t firstX, bool freeX) {
+    size = x;
+    first = firstX;
+    isfree = freeX;
+    prev = next = nullptr;
+  }
+
+};
+
+class MemoryManager
+  {
+    public:
+      MemoryManager(unsigned wordSize, function<int(int, void *)> allocator);
+      ~MemoryManager();
+      void initialize(size_t sizeInWords);
+      void shutdown();
+      void* allocate(size_t sizeInBytes);
+      void free(void *address);
+      void setAllocator(function<int(int, void *)> allocator);
+      int dumpMemoryMap(char *filename);
+      void *getList();
+      void *getBitmap();
+      unsigned getWordSize();
+      void *getMemoryStart();
+      unsigned getMemoryLimit();
+    private:
+      uint8_t* memory = nullptr;
+      Block* head = nullptr;
+      int wordSize;
+      int sizeinwords;
+      function<int(int, void *)> defaultAllocator;
+  };
+
+int bestFit(int sizeInWords, void *list);
+int worstFit(int sizeInWords, void *list);
