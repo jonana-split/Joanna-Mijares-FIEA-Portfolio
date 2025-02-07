e8bf75632cd6405c3fafcd28f89ded420b2eb2bf
Joanna Mijares <127276862+jonana-split@users.noreply.github.com>
2025-02-07 15:01:35 -0500

    Update MemoryManager.cpp

--- MemoryManager/MemoryManager.cpp
+++ MemoryManager/MemoryManager.cpp
@@ -15,25 +15,28 @@ using namespace std;
 
 // Joanna Mijares
 
+//store word size and store passed-in allocator function as member variable
 MemoryManager::MemoryManager(unsigned wordSize, function<int(int, void *)> allocator){
   this-> wordSize = wordSize;
   this-> defaultAllocator = allocator;
 }
-//store word size and store passed-in allocator function as member variable
 
+//if object is initialized, call shutdown
 MemoryManager::~MemoryManager(){
         shutdown();
         wordSize = 0;
         defaultAllocator = nullptr;
 }
-//if object is initialized, call shutdown
 
+//initialize block of size sizeInWords
 void MemoryManager::initialize(size_t sizeInWords){
 
+    //size limit
     if(sizeInWords > 65536) {
         return;
     }
 
+    //clean existing block
     if(head!=nullptr) {
         shutdown();
     }
@@ -52,14 +55,13 @@ void MemoryManager::initialize(size_t sizeInWords){
     head = new Block(sizeInBytes, 0, true);
     
 }
-//check documentation
 
 //CITE: https://www.geeksforgeeks.org/write-a-function-to-delete-a-linked-list/
 //CITE: https://stackoverflow.com/questions/52691072/created-a-linked-list-but-how-to-delete-all-the-nodes-in-c
 //CITE: https://www.alphacodingskills.com/cpp/ds/cpp-delete-all-nodes-of-the-doubly-linked-list.php
-void MemoryManager::shutdown(){
 
-    //blocks!
+//clears block data, frees heap mem, resets mem vars, clear data structures
+void MemoryManager::shutdown(){
 
     while(head != nullptr) {
         Block* current = head->next;
@@ -67,25 +69,24 @@ void MemoryManager::shutdown(){
         head = current;
     }
 
-    //member vars!
     head = nullptr;
     sizeinwords = 0;
-    //wordSize = 0; //might move to destructor
+
     delete[] memory;
     memory = nullptr;
-    //defaultAllocator = nullptr;
 
 }
-//clears block data, frees heap mem, resets mem vars, clear data structures
 
 void *MemoryManager::allocate(size_t sizeInBytes){
-    
+
     size_t numWords;
 
+    //there is nothing to allocate
     if(wordSize == 0){
         return 0;
     }
 
+    //calculate how many words
     if(sizeInBytes % wordSize > 0 ) {
          numWords = (sizeInBytes / wordSize) + 1;
     }else {
@@ -93,13 +94,16 @@ void *MemoryManager::allocate(size_t sizeInBytes){
     }
 
     //CITE: https://stackoverflow.com/questions/2622441/c-integer-floor-function
+    //get list of holes available 
     uint16_t* arr = static_cast<uint16_t *>(getList());
 
+    //if no memory or holes, return null
     if(arr == nullptr) {
         delete[] arr;
         return nullptr;
     }
 
+    //obtain offset
     int offset = defaultAllocator(numWords, arr);
     int offsetBytes = offset*wordSize;
 
@@ -108,17 +112,17 @@ void *MemoryManager::allocate(size_t sizeInBytes){
     //CITE: https://www.geeksforgeeks.org/delete-a-node-in-a-doubly-linked-list/
     //CITE: https://stackoverflow.com/questions/23718785/how-to-change-linkedlist-head-pointer-globally-and-not-locally
 
+    
     while(current != nullptr) {
 
-
+        //if allocated block is exact size of free block, allocate memory to entire block
         if(offset == current->first && sizeInBytes == current->size && current->isfree == true) {
             current->isfree = false;
             break;
         }else if(offset == current->first && sizeInBytes < current->size && current->isfree == true){
-            //if at the start!  
+            //if offset position is at the start, allocate memory just to start
             Block *newBlock = new Block(sizeInBytes, offset, false);
 
-            //this little if statement fixed so many issues ;-;
             if(current == head){
                 head = newBlock;
             }
@@ -137,7 +141,7 @@ void *MemoryManager::allocate(size_t sizeInBytes){
             break;
 
         }else if(offset > current->first && sizeInBytes < current->size && current->first + (current->size/wordSize) - numWords == offset && current->isfree == true){
-            //the end!
+            //if offset position is at the end, allocate just to the end of the block
 
             Block *newBlock = new Block(sizeInBytes, offset, false);
             newBlock->next = current->next;
@@ -149,7 +153,7 @@ void *MemoryManager::allocate(size_t sizeInBytes){
             break;
 
         }else if(offset > current->first && offset < current->next->first && sizeInBytes < current->size && current->first + (current->size/wordSize) - numWords < offset && current->isfree == true){
-            //in the middle
+            //if offset position is in the middle, allocate memory just to middle
 
             Block *newBlock = new Block(sizeInBytes, offset, false);
             Block *endBlock = new Block(current->size-sizeInBytes, offset+numWords, true);
@@ -167,20 +171,16 @@ void *MemoryManager::allocate(size_t sizeInBytes){
         current = current->next;
     }
 
-    //nothing to allocate :(
+    //nothing to allocate
     if(current == nullptr){
         delete[] arr;
         return nullptr;
     }
 
-    //delete!
+    //free memory
     delete[] arr;
 
     return (void*)(memory + offset*wordSize);
-
-    //bytes "rented out"
-    //allocate more and more heap memory
-    //free the memory getlist allocated (delete)
     //return a ptr in mem block to starting location of newly allocated space
 }
 
@@ -191,6 +191,7 @@ void *MemoryManager::allocate(size_t sizeInBytes){
 //CITE: https://stackoverflow.com/questions/70468653/traverse-a-doubly-linked-list-to-a-given-index
 //CITE: https://stackoverflow.com/questions/40219833/removing-nodes-from-linked-list-not-working-properly
 
+//free blocks of memory
 void MemoryManager::free(void *address){
 
     Block* current = head;
@@ -201,19 +202,19 @@ void MemoryManager::free(void *address){
         //if the address to free is within the current block (greater than start of block, less than end of block)
         if(address >= (void*)(memory+(current->first*wordSize)) && address < (void*)((memory+(current->first*wordSize)) + current->size)) {
 
-            //no neighbors
+            //if there are no neighbors, make free
             if(current->prev != nullptr && current->next != nullptr && current->next->isfree == false && current->prev->isfree == false){
                 current->isfree = true;
                 return;
             }
 
-            //if its the head + no neighbors
+            //if its the head + no neighbors, make head free
             if(head == current && current->next != nullptr && current->next->isfree == false){
                 current->isfree = true;
                 return;
             }
 
-            //if next is hole
+            //if next is hole, merge holes
             if(current->next != nullptr && current->next->isfree == true) {
                 current->isfree = true;
                 Block* temp = current->next;
@@ -225,7 +226,7 @@ void MemoryManager::free(void *address){
                 delete temp;
               
             }
-            //if prev is hole
+            //if prev is hole, merge holes
             if(current->prev != nullptr && current->prev->isfree == true) {
                 current->isfree = true;
                 Block* temp = current->prev;
@@ -251,18 +252,16 @@ void MemoryManager::free(void *address){
             return;
         }
     }
-    //TO DO:
-    // addr is starting loc of space to be freed
-    //will match smth from allocate in the last
-    //if block freed, prev hole gets extended
-    //freeing block in middle != new hole
+  
     return;
 }
 
+//change allocation algorithm to selected algorithm, used for testing
 void MemoryManager::setAllocator(function<int(int, void *)> allocator){
     this->defaultAllocator = allocator;
 }
 
+//POSIX calls to write a list of homes, format [offset/start , length]
 int MemoryManager::dumpMemoryMap(char *filename){
 
     //CITE: https://www.geeksforgeeks.org/input-output-system-calls-c-create-open-close-read-write/
@@ -274,6 +273,7 @@ int MemoryManager::dumpMemoryMap(char *filename){
         return -1;
     }
 
+    //get list of holes
     uint16_t* arr = static_cast<uint16_t *>(getList());
 
     //CITE: https://stackoverflow.com/questions/11063748/output-commas-between-elements
@@ -284,6 +284,7 @@ int MemoryManager::dumpMemoryMap(char *filename){
     char buffer[1024];
     int strSize;
 
+    //write offsets and lengths from list of holes
     for(int i=1; i<=(2*arr[0]); i++) {
 
         if(i % 2 == 0 && i != arr[0]*2) {
@@ -298,14 +299,14 @@ int MemoryManager::dumpMemoryMap(char *filename){
     close(fd);
 
     delete[] arr;
-
-
-  return 0;
+  
+    return 0;
 }
 
-//getlist.. scary
+//get list of holes
 void *MemoryManager::getList(){
 
+    //if no holes or memory allocated, return null
     if(!head) {
         return nullptr;
     }
@@ -321,6 +322,7 @@ void *MemoryManager::getList(){
     //go through every block
     while (current != nullptr) {
         //number of holes AKA arr[0]
+        //increase number of holes if current is free
         if(current->isfree){
             numHoles ++;
         }
@@ -334,22 +336,23 @@ void *MemoryManager::getList(){
         return nullptr;
     }
 
+    //first item in array is number of holes
     arr[0] = numHoles;
 
     if(numHoles == 0){
         return arr;
     }
     
-    //add values to arr
-    //this is incorrect rn
     Block* curr = head;
 
     int arrIndex = 1;
 
+    //while current exists
     while (curr != nullptr) {
 
         int arrSize = 0;
-        
+
+        //get how many
         if(wordSize != 0 && curr->size !=0){
             arrSize = (curr->size)/wordSize;
         }
